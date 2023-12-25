4f7963aa450acb303fd47d5c174bbb132099715d
Ranika23 <bveronikal@gmail.com>
2023-12-25 03:51:01 +0300

    feat: add solution of task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,83 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(value = true) {
+    this.value = value;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  error(message, key) {
+    if (message === undefined ||
+      message === null ||
+      message === '' ||
+      key === undefined ||
+      key === null ||
+      key === '') throw new Error('Incorrect arguments!');
+  }
+
+  encrypt(message, key) {
+    this.error(message, key)
+
+    let kf = Math.ceil(message.length / key.length)
+    key = key.repeat(kf);
+
+    let arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = [];
+    let n = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(message[i].toUpperCase()) === -1) {
+        result.push(message[i])
+      } else {
+        let letterIdx = arr.indexOf(message[i].toUpperCase());
+        let shift = arr.indexOf(key[n].toUpperCase());
+        n++;
+
+        result.push(arr[(letterIdx + shift) % 26])
+      }
+    }
+
+    let res = result;
+    if (this.value === false) {
+      res.reverse()
+    }
+    return res.join('')
+
+  }
+
+  decrypt(encryptedMessage, key) {
+    this.error(encryptedMessage, key)
+
+    let kf = Math.ceil(encryptedMessage.length / key.length)
+    key = key.repeat(kf);
+
+    let arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let n = 0;
+    let result = [];
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(encryptedMessage[i].toUpperCase()) === -1) {
+        result.push(encryptedMessage[i])
+      } else {
+
+        let letterIdx = arr.indexOf(encryptedMessage[i].toUpperCase());
+        let shift = arr.indexOf(key[n].toUpperCase());
+        n++;
+
+        result.push(arr[(letterIdx - shift + 26) % 26])
+      };
+    }
+
+    let res = result;
+    if (this.value === false) {
+      res.reverse()
+    }
+    return res.join('')
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
